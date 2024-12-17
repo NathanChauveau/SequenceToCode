5687e918aff581dd15e37cc3d1ef4e9ae0f4be70
Nathan Chauveau <nathan.chauveau@eduvaud.ch>
2024-12-17 10:42:36 +0100

    feat: add correction

--- src corrigé/Person.cs
+++ src corrigé/Person.cs
@@ -1,11 +1,18 @@
 namespace Carfleet
 {
-    public class Vehicule
+    public class Person
     {
         #region private attributes
+        private _emailaddress = string
         #endregion private attributes
 
         #region public methods
+        public function Person(string : emailaddress)
+        {
+            _emailaddress = emailaddress;
+        }
+
+        public string emailaddress { get => _emailaddress; }
 
         #endregion public methods
 
