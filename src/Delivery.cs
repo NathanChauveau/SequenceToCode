3cdaae40b65ac5e07702e813353b47b89a0beaf2
Nathan Chauveau <nathan.chauveau@eduvaud.ch>
2025-01-14 12:25:45 +0100

    feat: add missing class

---
+++ src/Delivery.cs
@@ -0,0 +1,16 @@
+namespace Carfleet
+{
+    public class Delivery
+    {
+        private _hazardousMaterial = bool
+        private _material = string[]
+
+
+        public function Delivery(bool hazardousMaterial, string[] material)
+        {
+            _hazardousMaterial = hazardousMaterial;
+            _material = material;
+        }
+
+    }
+}
